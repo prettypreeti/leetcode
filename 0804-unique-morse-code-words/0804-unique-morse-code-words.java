a017b2135aaac970d777d86ff6b1fd3491a4a05d
prettypreeti <157099371+prettypreeti@users.noreply.github.com>
2025-03-18 15:14:44 +0530

    Time: 3 ms (36.11%), Space: 41.7 MB (67.38%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+       String[] str = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        Set<String> set  = new HashSet<>();
+        
+        for(String s:words){
+            StringBuilder sb = new StringBuilder();
+            for(char c:s.toCharArray()){
+                sb.append(str[c-'a']);
+            }
+            set.add(sb.toString());
+        }
+        System.out.println(str.length);
+        return set.size(); 
+    }
+}
